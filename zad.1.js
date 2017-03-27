f72b38b831e1573085ba8fe096ddb8cc34a8d8ce
ewa <szsz.ewa@gmail.com>
2017-03-27 16:42:14 +0200

    dodaje plik index.html

---
+++ zad.1.js
@@ -0,0 +1,8 @@
+var name = prompt('Wpisz swoje imię');
+// alert('Witaj, ' + name); to jest komentarz 
+console.log('Witaj, ' + name);
+/* to jest komentarz l;l,;l,;l,
+lkmlkmlkmlkm/*
+/*ljnkjnkjnkjnkjnkjnkjn
+lkmlkmlkmlkm
+lkmlkmlkm/* 
